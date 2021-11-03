d6e0c21c6d283bf10ab5520ca36b65b1c891c6a8
vlatona <93345610+vlatona@users.noreply.github.com>
2021-11-03 10:56:11 -0700

    Appointment class uploaded
    This class will need to be updated upon the commitment of future classes.

---
+++ Appointment.java
@@ -0,0 +1,159 @@
+import java.util.*; //Use of ArrayLists
+
+//Class Appointment for Doctors and Nurses
+public class Appointment 
+{
+
+	// Private variables
+	private String height;
+	private int weight;
+	private String bloodPressure;
+	private ArrayList<String> allergies;
+	private ArrayList<String> concerns;
+	private ArrayList<String> newMeds;
+	private String summary;
+	private Patient patient;
+
+	//Constructor
+	public Appointment(String h, int w, String bp, Patient p) 
+	{
+
+		// Set private variables
+		height = h;
+		weight = w;
+		bloodPressure = bp;
+		patient = p;
+
+	}//End of constructor
+
+	//Public methods
+	//Mutators
+	//Set Height
+	public boolean setHeight(String h) 
+	{
+
+		//Set height and return true
+		height = h;
+		return true;
+
+	}//End of method setHeight
+
+	//Set Weight
+	public boolean setWeight(int w) 
+	{
+
+		//Set weight and return true
+		weight = w;
+		return true;
+
+	}//End of method setWeight
+
+	//Set BP
+	public boolean setBP(String bp) 
+	{
+
+		//Set BP and return true
+		bloodPressure = bp;
+		return true;
+
+	}//End of method setBP
+
+	//Set Patient
+	public boolean setPatient(Patient p) 
+	{
+
+		//Set patient and return true
+		patient = p;
+		return true;
+
+	}//End of method setPatient
+
+	//Set Summary
+	public boolean setSummary(String s) 
+	{
+
+		//Set summary and return true
+		summary = s;
+		return true;
+
+	}//End of method setSummary
+
+	//Add Allergy
+	public boolean addAllergy(String allergy) 
+	{
+
+		//Add allergy and return true/false
+		boolean bool = allergies.add(allergy);
+		return bool;
+
+	}//End of method addAllergy
+
+	//Remove Allergy
+	public boolean removeAllergy(String allergy) 
+	{
+
+		//Remove allergy and return true/false
+		boolean bool = allergies.remove(allergy);
+		return bool;
+
+	}//End of method removeAllergy
+
+	//Add Concern
+	public boolean addConcern(String concern) 
+	{
+
+		//Add concern and return true/false
+		boolean bool = concerns.add(concern);
+		return bool;
+
+	}//End of method addConcern
+
+	//Remove Concern
+	public boolean removeConcern(String concern) 
+	{
+
+		//Remove concern and return true/false
+		boolean bool = concerns.remove(concern);
+		return bool;
+
+	}//End of method removeConcern
+	
+	//Add Med
+	public boolean addMed(String med)
+	{
+		
+		//Add new med and return true/false
+		boolean bool = newMeds.add(med);
+		return bool;
+		
+	}//End of method addMed
+	
+	//Remove Med
+	public boolean removeMed(String med)
+	{
+		
+		//Remove new med and return true/false
+		boolean bool = newMeds.remove(med);
+		return bool;
+		
+	}//End of method removeMed
+	
+	//Update History
+	public boolean updateHistory()
+	{
+		
+		
+		
+	}//End of method updateHistory
+	
+	//Send Prescriptions
+	public boolean sendPrescriptions(PrescriptionHandler ph)
+	{
+		
+		//Send prescriptions to pharmacy and return true/false
+		boolean bool = ph.sendToPharmacy(patient.getPharmacy(), newMeds);
+		return bool;
+		
+	}//End of method sendPrescriptions
+
+}//End of class Appointment
