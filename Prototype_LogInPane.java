47e489d28e2e0a16303cac97b6aad9b3cedab0b2
Akhann-21 <89493370+Akhann-21@users.noreply.github.com>
2021-11-05 13:09:45 -0700

    Add files via upload

--- Prototype_LogInPane.java
+++ Prototype_LogInPane.java
@@ -1,24 +1,36 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class Prototype_LogInPane extends BorderPane {
 
-	Label title, userID, password;
-	TextField userIDInput, passwordInput;
+	String testUser = "Aryan Khanna";
+	String testPass = "Aryank2001";
+	String checkUser, checkPass;
 	
+	Label title, userID, password;
+	TextField userIDInput;
+	PasswordField passwordInput;
+	
+	Label message;
 	Button logIn;
 	
 	HBox hPane, hPane2;
 	VBox vPane;
-	BorderPane bPane;
-	
+	TabPane tPane;
+	GridPane gPane;
+
 	public Prototype_LogInPane() {
 		
 		title = new Label("Portal Log In: ");
@@ -26,7 +38,9 @@ public class Prototype_LogInPane extends BorderPane {
 		password = new Label("Enter Password: ");
 		
 		userIDInput = new TextField();
-		passwordInput = new TextField();
+		passwordInput = new PasswordField();
+		
+		message = new Label();
 		
 		logIn = new Button("Log In");
 		//logIn.setPadding(new Insets(10, 20, 10, 20));
@@ -37,27 +51,30 @@ public class Prototype_LogInPane extends BorderPane {
 		hPane.getChildren().addAll(userID, userIDInput);
 		
 		hPane2 = new HBox();
-		hPane2.setSpacing(20);
+		hPane2.setSpacing(34);
 		hPane2.getChildren().addAll(password, passwordInput);
+		
+		gPane = new GridPane();
+		gPane.setPadding(new Insets(40, 40, 40, 40));
+		gPane.setHgap(25);
+		gPane.setVgap(25);
+
+		//gPane.add(title, 0, 1);
+		gPane.add(userID, 0, 0);
+		gPane.add(userIDInput, 1, 0);
+		gPane.add(password, 0, 1);
+		gPane.add(passwordInput, 1, 1);
+		//gPane.add(logIn, 2, 4);
+		//gPane.add(message, 2, 5);
 		
 		vPane = new VBox();
 		vPane.setSpacing(20);
-		vPane.setPadding(new Insets(100, 50, 10, 100));
-		vPane.getChildren().addAll(title, hPane, hPane2, logIn);
-	
+		vPane.setPadding(new Insets(100, 100, 100, 100));
+		vPane.setAlignment(Pos.CENTER);
+		vPane.getChildren().addAll(title, gPane, logIn, message);
 		
-		/*bPane = new BorderPane();
-		bPane.setTop(title);
-		bPane.setCenter(vPane);
-		bPane.setBottom(logIn);
 		
-		this.getChildren().add(bPane);
-		this.setPrefSize(500, 500);*/
-		
-		//this.setTop(title);
-		this.setCenter(vPane);
-		//this.setBottom(logIn);
-		
+		this.setCenter(vPane);	
 		
 	}
 	
@@ -66,8 +83,19 @@ public class Prototype_LogInPane extends BorderPane {
 		@Override
 		public void handle(ActionEvent event) {
 
+			checkUser = userIDInput.getText().toString();
+			checkPass = passwordInput.getText().toString();
 			
+			if(checkUser.equals(testUser) && checkPass.equals(testPass)) {
+				message.setText("Log in successful");
+				message.setTextFill(Color.GREEN);
+			} else {
+				message.setText("Incorrect Username or Password");
+				message.setTextFill(Color.RED);
+			}
 			
+			userIDInput.setText("");
+			passwordInput.setText("");
 		}	
 	}
 }
